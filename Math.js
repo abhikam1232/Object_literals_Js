135f3622badd6f489cee593762ac0413b31e41ac
Abhishek Kamble <abhikam152002@gmail.com>
2025-08-05 02:19:00 +0530

    Add files via upload

---
+++ Math.js
@@ -0,0 +1,23 @@
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.cos);
+console.log(Math.E);
+console.log(Math.abs(2));
+console.log(Math.abs(-3));
+console.log(Math.pow(2, 3));
+console.log(Math.floor(5.999)); // It helps to round of smallest 
+console.log(Math.floor(6.9999)); // It also helps to round of the smallest 
+
+// This Math object is used to round of the largest:- 
+console.log(Math.ceil(6.9999)); // Now it rounds of the largest value:- 
+console.log(Math.ceil(99.87554)); // It rounds of and takes t0 100
+
+
+// This is used to give any random value:- 
+
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
+console.log(Math.random());
