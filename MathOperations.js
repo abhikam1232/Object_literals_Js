b6c77aa5c697948733ede29c48600a987f7d1a4a
Abhishek Kamble <abhikam152002@gmail.com>
2025-08-07 13:38:07 +0530

    Add files via upload

---
+++ MathOperations.js
@@ -0,0 +1,32 @@
+ /** let myStep1 = Math.random();
+console.log(myStep1);
+
+let myStep2 = myStep1 * 10;
+console.log(myStep2);
+
+let step3 = Math.floor(myStep2);
+console.log(step3);
+
+step3 = step3 + 1;
+console.log(step3); **/ // This is the complete process tor print the random number but step by step: 
+
+
+Math.floor( Math.random()* 10) + 1; // This does the previous operation in one step:- 
+
+// Create a Random number between  1 to 100:- 
+console.log(Math.floor( Math.random()* 100) + 1);
+
+// Create random numbers between 1 to 5:-
+console.log(Math.floor( Math.random() * 5) + 1)
+
+// Create random numbers between 21 to 25:- 
+console.log( Math.floor( Math.random() * 20) + 5);
+
+// random between 1 to 100:- 
+console.log(Math.floor ( Math.random()* 100))+1;
+
+// random numbers between 21 to 25:-
+console.log( Math.floor( Math.random()*5) + 20);
+
+// Random numbers between 41 to 45:-
+console.log( Math.floor( Math.random()*5) + 40);
